08c44cf87f699604e524a314edbf34121445fe8b
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-07-08 23:51:45 +0200

    Company creation - PART 2

--- src/main/java/me/andrej123456789/stocksigns/stock_market/CreateCompany.java
+++ src/main/java/me/andrej123456789/stocksigns/stock_market/CreateCompany.java
@@ -1,6 +1,13 @@
 package me.andrej123456789.stocksigns.stock_market;
 
 import org.bukkit.ChatColor;
+import org.bukkit.conversations.Conversation;
+import org.bukkit.conversations.ConversationAbandonedEvent;
+import org.bukkit.conversations.ConversationAbandonedListener;
+import org.bukkit.conversations.ConversationContext;
+import org.bukkit.conversations.ConversationFactory;
+import org.bukkit.conversations.Prompt;
+import org.bukkit.conversations.StringPrompt;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -14,6 +21,9 @@ import me.andrej123456789.stocksigns.StockSigns;
 public class CreateCompany {
 
     private static final Plugin plugin = JavaPlugin.getProvidingPlugin(StockSigns.class);
+
+    private String confirmation_message_question = "";
+    private String confirmation_message_answer = "";
 
     /**
      * Create a company
@@ -41,10 +51,12 @@ public class CreateCompany {
         Integer owner_stocks = 0;
         Integer stock_price = 0;
 
+        String owner = player.getName();
+
         // Parse line 1
         try {
-            company_code = lines[1].split(",", 2)[0];
-            stock_exchange_code = lines[1].split(",", 2)[1];
+            company_code = lines[1].split(",", 2)[0].toUpperCase();
+            stock_exchange_code = lines[1].split(",", 2)[1].toUpperCase();
         } catch (ArrayIndexOutOfBoundsException e) {
             player.sendMessage(ChatColor.RED + "There was error splitting second line.\n" + 
                                 "Please check if comma is included!");
@@ -106,9 +118,45 @@ public class CreateCompany {
             return;
         }
 
-        player.sendMessage(company_name);
-        player.sendMessage(company_code + " | " + stock_exchange_code);
-        player.sendMessage(max_stocks.toString() + " | " + owner_stocks.toString());
-        player.sendMessage(stock_price.toString());
+        confirmation_message_question = ChatColor.LIGHT_PURPLE + "Confirm following settings: \n" + ChatColor.RESET + 
+                                "Name: " + company_name + "\n" + 
+                                "Code: " + company_code + "\n" + 
+                                "Number of stocks: " + max_stocks.toString() + "\n" + 
+                                "Owner '" + owner + "' owning " + owner_stocks.toString() + "\n" + 
+                                "Price per stock: " + stock_price.toString() + "\n" + 
+                                ChatColor.AQUA + "You can cancel operation by typing 'cancel' or change owner by typing " + 
+                                "'o:new_owner'.\nIf however, everything is ok, just send random letter within next 10 seconds.";
+
+        ConversationFactory factory = new ConversationFactory(plugin)
+            .withModality(true)
+            .withFirstPrompt(new QuestionPrompt())
+            .withEscapeSequence("cancel")
+            .withLocalEcho(false)
+            .thatExcludesNonPlayersWithMessage("Only players can use this conversation")
+            .withTimeout(10);
+            
+        Conversation conversation = factory.buildConversation(player);
+        conversation.begin();
+
+        if (confirmation_message_answer.startsWith("o:")) {
+            owner = confirmation_message_answer.substring(2, confirmation_message_answer.length());
+        }
+
+        player.sendMessage(owner);
+    }
+
+    private class QuestionPrompt extends StringPrompt {
+        @Override
+        public String getPromptText(ConversationContext context) {
+            return confirmation_message_question;
+        }
+
+        @Override
+        public Prompt acceptInput(ConversationContext context, String input) {
+            confirmation_message_answer = input;
+
+            context.getForWhom().sendRawMessage(ChatColor.DARK_AQUA + "Confirmed!");
+            return END_OF_CONVERSATION;
+        }
     }
 }
